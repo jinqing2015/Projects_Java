65d4ca36ad2da86da12bea4eb7f1ba17c6caa579
zjq <903275109@qq.com>
2016-04-03 11:33:35 +0800

    finish the observer schema

---
+++ Desgin Schema/src/cn/schema/observer/DisplayElement.java
@@ -0,0 +1,7 @@
+package cn.schema.observer;
+
+public interface DisplayElement {
+	
+	public void display();
+
+}
---
+++ Desgin Schema/src/cn/schema/observer/Subject.java
@@ -0,0 +1,9 @@
+package cn.schema.observer;
+
+public interface Subject {
+	
+	public void registerObserver(Observer o);
+	public void removeObserver(Observer o);
+	public void notifyObservers();
+	
+}
